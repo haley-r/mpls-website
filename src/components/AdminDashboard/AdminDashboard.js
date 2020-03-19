a515d9c8dc8072d076515297f2ecf8c94527dbd2
haley-r <haleyryan77@gmail.com>
2020-03-19 11:33:46 -0500

    logout button reminder

--- src/components/AdminDashboard/AdminDashboard.js
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 //turn this into a class component, lol
 
 const AdminDashboard = () => (
-    <p>this is where the admin dashboard will go. u can only see if logged in!</p>
+    <p>this is where the admin dashboard will go. u can only see if logged in! I need to add a logout button (when logged in) somewhere- see starter code for that conditional rendering</p>
 );
 
 export default AdminDashboard;
