f1a2c1e6b5791350be3029074e840ab0f3b4fdff
haley-r <haleyryan77@gmail.com>
2020-03-24 22:06:32 -0500

    edited form a bit more

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -25,12 +25,55 @@ class EventForm extends Component {
     render() {
         return (
             <section className="submitEvent">
-                <h2>fill in these details!</h2>
                 <form onSubmit={this.createEvent}>
-                    <input onChange={(event)=>this.handleInput(event, 'title')}  placeholder="name of event"/>
-                    <input onChange={(event) => this.handleInput(event, 'description')}placeholder="description of event" />
+                    <h1>fill in these details!</h1>
+                    <p>Since anyone can make an event post without making an account, you won't be able to edit your post after submitting it. Think of it like making a poster to leave around town.</p>
+                    <p>An admin will check your post for completion and compliance with general community standards (see 'about') before publishing it.</p>
+                        <label htmlFor="name">
+                            Name of Event:
+                                <input
+                                type="text"
+                                name="name"
+                                placeholder="short but specific event name"
+                                value={this.state.username}
+                                onChange={(event) => this.handleInput(event, 'title')}
+                                />
+                        </label>
+                        <label htmlFor="date">
+                            Date of Event:
+                                    <input
+                                type="date"
+                                name="date"
+                                placeholder="short but specific event name"
+                                value={this.state.username}
+                                onChange={(event) => this.handleInput(event, 'title')}
+                            />
+                        </label>
+                        <label htmlFor="time">
+                            Time of Event:
+                                        <input
+                                type="time"
+                                name="time"
+                                placeholder="short but specific event name"
+                                value={this.state.username}
+                                onChange={(event) => this.handleInput(event, 'title')}
+                            />
+                        </label>
+                        <label htmlFor="description">
+                            Description:
+                                <textarea
+                                type="text"
+                                name="description"
+                                placeholder="a description of your event"
+                                value={this.state.username}
+                                onChange={(event) => this.handleInput(event, 'description')}
+                                />
+                        </label>
+
+
+
                     <input
-                        className="log-in"
+                        className="input-button"
                         type="submit"
                         name="submit"
                         value="submit event"
